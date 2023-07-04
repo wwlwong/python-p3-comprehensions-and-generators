c0dde47a3ba0d0556f775674b2531d982d6a6d32
William Wong <william.wai.lim.wong@gmail.com>
2023-07-04 14:41:14 -0400

    completes comprehensions and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [num for num in num_list if num % 2 == 0]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [sentence+"!" for sentence in sentence_list]
+    return new_list
